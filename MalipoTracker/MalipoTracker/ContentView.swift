d423ed50e24ac5e3bcf38bf8d1a702e0ea0a7fcb
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-02-17 23:02:07 +0200

    Add Initial project fiels

---
+++ MalipoTracker/MalipoTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MalipoTracker
+//
+//  Created by Cédric Bahirwe on 17/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MalipoTracker/MalipoTracker/MalipoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MalipoTrackerApp.swift
+//  MalipoTracker
+//
+//  Created by Cédric Bahirwe on 17/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct MalipoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
