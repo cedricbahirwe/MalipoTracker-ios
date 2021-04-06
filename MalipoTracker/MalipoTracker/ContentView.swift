f8873b5166be81b807694e8e295ebb20a7206085
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-04-06 09:14:12 +0200

    Minor Update

--- MalipoTracker/MalipoTracker/ContentView.swift
+++ MalipoTracker/MalipoTracker/ContentView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TestingView()
-//        NavigationView {
-//            HomeView()
-//                .navigationTitle("")
-//                .navigationBarHidden(true)
-//        }
+//        TestingView()
+        NavigationView {
+            HomeView()
+                .navigationTitle("")
+                .navigationBarHidden(true)
+        }
     }
 }
 
