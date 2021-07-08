0f8ce54d7bcfcb76e668ed48669a03c7ad0c38f2
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-07-08 23:41:25 +0200

    remove unused

--- MalipoTracker/MalipoTracker/ChartView.swift
+++ MalipoTracker/MalipoTracker/ChartView.swift
@@ -54,47 +54,39 @@ struct BarChart: View {
     @State private var currentValue = ""
     @State private var currentLabel = ""
     @State private var touchLocation: CGFloat = -1
-
+    
     var body: some View {
         VStack(alignment: .leading) {
-//            Text(title)
-//                .font(Font.largeTitle.bold())
-//            Text("Current value: \(currentValue)")
-//                .font(.headline)
-//            Spacer()
-            
             GeometryReader { geometry in
                 VStack(spacing: 10) {
                     LinearGradient(gradient: Gradient(colors: [Color.green, Color.blue, Color.red]), startPoint: .bottom, endPoint: .top)
                         .mask(
-                    HStack {
-                        ForEach(0..<data.count, id: \.self) { i in
-                            BarChartCell(value: normalizedValue(index: i), barColor: barColor)
-                                .opacity(barIsTouched(index: i) ? 1 : 0.7)
-                                .scaleEffect(barIsTouched(index: i) ? CGSize(width: 1.05, height: 1) : CGSize(width: 1, height: 1), anchor: .bottom)
-                                .animation(.spring())
-                                .padding(.top)
-                        }
-                    }
-                    .gesture(DragGesture(minimumDistance: 0)
-                        .onChanged({ position in
-                            let touchPosition = position.location.x/geometry.frame(in: .local).width
-                            touchLocation = touchPosition
-                            
-                            updateCurrentValue()
-
-                        })
-                        .onEnded({ _ in
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                withAnimation(Animation.easeOut(duration: 0.5)) {
-                                   resetValues()
+                            HStack {
+                                ForEach(0..<data.count, id: \.self) { i in
+                                    BarChartCell(value: normalizedValue(index: i), barColor: barColor)
+                                        .opacity(barIsTouched(index: i) ? 1 : 0.7)
+                                        .scaleEffect(barIsTouched(index: i) ? CGSize(width: 1.05, height: 1) : CGSize(width: 1, height: 1), anchor: .bottom)
+                                        .animation(.spring())
+                                        .padding(.top)
                                 }
-                             }
-                        })
-                    )
-                    
-                    
-                    )
+                            }
+                        )
+                        .gesture(DragGesture(minimumDistance: 0)
+                                    .onChanged({ position in
+                                        let touchPosition = position.location.x/geometry.frame(in: .local).width
+                                        touchLocation = touchPosition
+                                        
+                                        updateCurrentValue()
+                                        
+                                    })
+                                    .onEnded({ _ in
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                            withAnimation(Animation.easeOut(duration: 0.5)) {
+                                                resetValues()
+                                            }
+                                        }
+                                    })
+                        )
                     if currentLabel.isEmpty {
                         Text(legend)
                             .bold()
@@ -121,21 +113,21 @@ struct BarChart: View {
     }
     
     func normalizedValue(index: Int) -> Double {
-             var allValues: [Double]    {
-                 var values = [Double]()
-                 for data in data {
-                     values.append(data.value)
-                 }
-                 return values
-             }
-             guard let max = allValues.max() else {
-                 return 1
-             }
-             if max != 0 {
-                 return Double(data[index].value)/Double(max)
-             } else {
-                 return 1
-             }
+        var allValues: [Double]    {
+            var values = [Double]()
+            for data in data {
+                values.append(data.value)
+            }
+            return values
+        }
+        guard let max = allValues.max() else {
+            return 1
+        }
+        if max != 0 {
+            return Double(data[index].value)/Double(max)
+        } else {
+            return 1
+        }
     }
     
     func barIsTouched(index: Int) -> Bool {
@@ -154,19 +146,19 @@ struct BarChart: View {
     }
     
     func resetValues() {
-             touchLocation = -1
-             currentValue  =  ""
-             currentLabel = ""
+        touchLocation = -1
+        currentValue  =  ""
+        currentLabel = ""
     }
     
     func labelOffset(in width: CGFloat) -> CGFloat {
-             let currentIndex = Int(touchLocation * CGFloat(data.count))
-             guard currentIndex < data.count && currentIndex >= 0 else {
-                 return 0
-             }
-             let cellWidth = width / CGFloat(data.count)
-             let actualWidth = width -    cellWidth
-             let position = cellWidth * CGFloat(currentIndex) - actualWidth/2
-             return position
+        let currentIndex = Int(touchLocation * CGFloat(data.count))
+        guard currentIndex < data.count && currentIndex >= 0 else {
+            return 0
+        }
+        let cellWidth = width / CGFloat(data.count)
+        let actualWidth = width -    cellWidth
+        let position = cellWidth * CGFloat(currentIndex) - actualWidth/2
+        return position
     }
 }
